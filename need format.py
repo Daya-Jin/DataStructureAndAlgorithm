271b9c824a49fed29fddc95b2364b3576db59d77
Daya_Jin <qq435248055@gmail.com>
2019-01-14 09:24:24 +0800

    update

--- need format.py
+++ need format.py
@@ -1,30 +1,26 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+def re_match(string, pattern):
+    # 几种特殊情况
+    if string is None or pattern is None:
+        return False
+    elif len(string) == 0 and len(pattern) == 0:
+        return True
+    elif len(string) > 0 and len(pattern) == 0:
+        return False
 
+    # 匹配位置后面出现"*"的情况，"*"的优先级要高于"."
+    elif len(pattern) > 1 and pattern[1] == '*':
+        # 分为两种情况，可以匹配或无法匹配
+        if len(string) > 0 and (string[0] == pattern[0] or pattern[0] == '.'):  # 可以匹配，字符相等或出现任意符
+            return (re_match(string, pattern[2:]) or  # 跳过'*'
+                    re_match(string[1:], pattern[2:]) or  # '*'匹配一次
+                    re_match(string[1:], pattern))  # '*'匹配多次
+        else:  # 无法匹配
+            return re_match(string, pattern[2:])
 
-# 设立三个指针，head，pre，cur，分别为头指针，最后一个非重复元素，工作指针
-# 注意始终保持pre->cur的链接
-def deleteDuplication(headnode):
-    if headnode is None or headnode.next is None:  # 空链表或单节点链表
-        return headnode
+    # 匹配成功的情况，字符相等或遇到任意匹配符"."
+    elif len(string) > 0 and (string[0] == pattern[0] or pattern[0] == '.'):
+        return re_match(string[1:], pattern[1:])
 
-    pre = None
-    cur = headnode  # 工作指针从头指针之前开始
-
-    while cur:  # 节点非空
-        if cur.next and cur.val == cur.next.val:  # 如果当前跟后继重复
-            dump = cur.val
-            while cur and cur.val == dump:  # 跳过所有等于重复值的节点
-                if pre is None:  # 头结点重复
-                    cur = cur.next
-                    headnode = cur
-                else:  # 中间节点重复
-                    cur = cur.next
-                    pre.next = cur
-        else:  # 当前节点未重复
-            pre = cur
-            cur = cur.next
-
-    return headnode
+    # 字符不等
+    else:
+        return False
