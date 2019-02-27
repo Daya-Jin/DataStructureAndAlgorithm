94635fb503e4a6d1b394766990857ec53c464b6c
Daya_Jin <qq435248055@gmail.com>
2019-02-27 15:36:26 +0800

    update

--- need format.py
+++ need format.py
@@ -1,43 +1,10 @@
-class Solution:
+def trailingZeroes(n):
+    res = 0
+    while n >= 5:
+        res += n // 5
+        n //= 5
+
+    return res
 
 
-    def findMode(self, root: 'TreeNode') -> 'List[int]':
-
-
-    if not root:
-    return []
-a = self.bitree(root)
-N = len(a)
-maxfreq = 1
-s = [a[0]]
-freq = 1
-for i in range(1, N):
-    if a[i] == a[i - 1]:
-    freq += 1
-if freq == maxfreq:
-    s.append(a[i])
-elif freq > maxfreq:
-s = [a[i]]
-maxfreq = freq
-else:
-freq = 1
-if maxfreq == 1:
-    s.append(a[i])
-return s
-
-
-def bitree(self, root):
-
-
-    if not root:
-    return []
-else:
-if root.left:
-    left = self.bitree(root.left)
-else:
-left = []
-if root.right:
-    right = self.bitree(root.right)
-else:
-right = []
-return left + [root.val] + right
+trailingZeroes(25)
