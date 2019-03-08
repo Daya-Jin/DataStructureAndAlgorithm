901731d8ead518eddcfcc98e2941f60feb0f1099
Daya_Jin <qq435248055@gmail.com>
2019-03-08 16:59:03 +0800

    update

--- need format.py
+++ need format.py
@@ -1,148 +1,24 @@
-"""
-Make the imports of python packages needed
-"""
-import pandas as pd
-import numpy as np
-from pprint import pprint
+# 思路：使用双指针往中心遍历即可
+# 假定字符串中还包含复杂字符，需要过滤
 
-dataset = pd.read_csv('zoo.csv',
-                      names=['animal_name', 'hair', 'feathers', 'eggs', 'milk',
-                             'airbone', 'aquatic', 'predator', 'toothed', 'backbone',
-                             'breathes', 'venomous', 'fins', 'legs', 'tail', 'domestic', 'catsize',
-                             'class', ])
-dataset = dataset.drop('animal_name', axis=1)
+def isPalindrome(s):
+    if len(s) < 2:
+        return True
 
+    l_idx, r_idx = 0, len(s) - 1
+    while l_idx < r_idx:
+        while not s[l_idx].isalnum and l_idx < r_idx:
+            l_idx += 1
+        while not s[r_idx].isalnum and l_idx < r_idx:
+            r_idx -= 1
 
-###########################################################################################################
-def entropy(target_col):
-    elements, counts = np.unique(target_col, return_counts=True)
-    entropy = np.sum(
-        [(-counts[i] / np.sum(counts)) * np.log2(counts[i] / np.sum(counts)) for i in range(len(elements))])
-    return entropy
+        if s[l_idx].lower() != s[r_idx].lower():
+            return False
 
-###########################################################################################################
+        l_idx += 1
+        r_idx -= 1
 
-###########################################################################################################
-def InfoGain(data, split_attribute_name, target_name="class"):
-    total_entropy = entropy(data[target_name])
-
-    vals, counts = np.unique(data[split_attribute_name], return_counts=True)
-
-    Weighted_Entropy = np.sum(
-        [(counts[i] / np.sum(counts)) * entropy(data.where(data[split_attribute_name] == vals[i]).dropna()[target_name])
-         for i in range(len(vals))])
-
-    Information_Gain = total_entropy - Weighted_Entropy
-    return Information_Gain
-
-###########################################################################################################
-###########################################################################################################
-def ID3(data, originaldata, features, target_attribute_name="class", parent_node_class=None):
-    # If all target_values have the same value, return this value
-    if len(np.unique(data[target_attribute_name])) <= 1:
-        return np.unique(data[target_attribute_name])[0]
-
-    # If the dataset is empty, return the mode target feature value in the original dataset
-    elif len(data) == 0:
-        return np.unique(originaldata[target_attribute_name])[
-            np.argmax(np.unique(originaldata[target_attribute_name], return_counts=True)[1])]
+    return True
 
 
-    elif len(features) == 0:
-        return parent_node_class
-
-
-    else:
-        # Set the default value for this node --> The mode target feature value of the current node
-        parent_node_class = np.unique(data[target_attribute_name])[
-            np.argmax(np.unique(data[target_attribute_name], return_counts=True)[1])]
-
-        # Select the feature which best splits the dataset
-        item_values = [InfoGain(data, feature, target_attribute_name) for feature in
-                       features]  # Return the information gain values for the features in the dataset
-        best_feature_index = np.argmax(item_values)
-        best_feature = features[best_feature_index]
-
-        # Create the tree structure. The root gets the name of the feature (best_feature) with the maximum information
-        # gain in the first run
-        tree = {best_feature: {}}
-
-        # Remove the feature with the best inforamtion gain from the feature space
-        features = [i for i in features if i != best_feature]
-
-        # Grow a branch under the root node for each possible value of the root node feature
-
-        for value in np.unique(data[best_feature]):
-            # print(best_feature,value)
-            value = value
-            # Split the dataset along the value of the feature with the largest information gain and therwith create sub_datasets
-            sub_data = data.where(data[best_feature] == value).dropna()
-
-            # Call the ID3 algorithm for each of those sub_datasets with the new parameters --> Here the recursion comes in!
-            subtree = ID3(sub_data, dataset, features, target_attribute_name, parent_node_class)
-
-            # Add the sub tree, grown from the sub_dataset to the tree under the root node
-            tree[best_feature][value] = subtree
-
-        return (tree)
-
-
-def predict(query, tree, default=1):
-    # 1.
-    for key in list(query.keys()):
-        if key in list(tree.keys()):
-            # 2.
-            try:
-                result = tree[key][query[key]]
-            except:
-                return default
-
-            # 3.
-            result = tree[key][query[key]]
-            # 4.
-            if isinstance(result, dict):
-                return predict(query, result)
-            else:
-                return result
-
-
-"""
-Check the accuracy of our prediction.
-The train_test_split function takes the dataset as parameter which should be divided into
-a training and a testing set. The test function takes two parameters, which are the testing data as well as the tree model.
-"""
-
-
-###########################################################################################################
-###########################################################################################################
-def train_test_split(dataset):
-    training_data = dataset.iloc[:80].reset_index(drop=True)  # We drop the index respectively relabel the index
-    # starting form 0, because we do not want to run into errors regarding the row labels / indexes
-    testing_data = dataset.iloc[80:].reset_index(drop=True)
-    return training_data, testing_data
-
-
-training_data = train_test_split(dataset)[0]
-testing_data = train_test_split(dataset)[1]
-
-
-def test(data, tree):
-    # Create new query instances by simply removing the target feature column from the original dataset and
-    # convert it to a dictionary
-    queries = data.iloc[:, :-1].to_dict(orient="records")
-
-    # Create a empty DataFrame in whose columns the prediction of the tree are stored
-    predicted = pd.DataFrame(columns=["predicted"])
-
-    # Calculate the prediction accuracy
-    for i in range(len(data)):
-        predicted.loc[i, "predicted"] = predict(queries[i], tree, 1.0)
-    print('The prediction accuracy is: ', (np.sum(predicted["predicted"] == data["class"]) / len(data)) * 100, '%')
-
-
-"""
-Train the tree, Print the tree and predict the accuracy
-"""
-tree = ID3(training_data, training_data, training_data.columns[:-1])
-pprint(tree)
-# test(testing_data, tree)
+isPalindrome("A man, a plan, a canal: Panama")
