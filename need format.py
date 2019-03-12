7013379c7285cd459170d907325aca60b7692bca
Daya_Jin <qq435248055@gmail.com>
2019-03-12 20:30:33 +0800

    update

--- need format.py
+++ need format.py
@@ -1,29 +1,29 @@
-# 思路：设置新数组，原数组设双指针，逐位比较元素并插入新数组
+# 思路：俯视图的面积最容易，为立方柱的个数，即求矩阵中非零元素的个数
+# 两个侧视图，分别对应矩阵每行最大值的和与每列最大值的和
 
-def merge(nums1, m, nums2, n):
-    if not nums1:
-        return nums2
-    if not nums2:
-        return nums1
+def projectionArea(grid) -> int:
+    if not grid:
+        return None
 
-    tmp_arr = [None] * (len(nums1) + len(nums2))
-    idx1 = idx2 = idx_tmp = 0
+    n = len(grid)
 
-    while idx1 < len(nums1) and idx2 < len(nums2):
-        if nums1[idx1] < nums2[idx2]:
-            tmp_arr[idx_tmp] = nums1[idx1]
-            idx1 += 1
-        else:
-            tmp_arr[idx_tmp] = nums2[idx2]
-            idx2 += 1
-        idx_tmp += 1
+    area = 0
+    for row in range(n):
+        row_max, col_max = 0, 0
+        for col in range(n):
+            if grid[row][col]:
+                area += 1
 
-    if idx1 == len(nums1) - 1:
-        tmp_arr[idx_tmp:] = nums2[idx2:]
-    if idx2 == len(nums2) - 1:
-        tmp_arr[idx_tmp:] = nums1[idx1:]
+            row_max = max(row_max, grid[row][col])
 
-    return tmp_arr
+            # 由于是立方矩阵，在行扫描的同时，交换行列坐标即可实现列扫描
+            row_col = max(col_max, grid[col][row])
+
+        area += row_max
+        area += col_max
+
+    return area
 
 
-merge([1, 2, 3], 3, [2, 5, 6], 3)
+projectionArea([[1, 2],
+                [3, 4]])
