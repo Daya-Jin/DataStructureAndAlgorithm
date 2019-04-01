3110463b88dd0c799a11c14d8742fd744501601a
Daya_Jin <qq435248055@gmail.com>
2019-04-01 20:56:48 +0800

    update

--- need format.py
+++ need format.py
@@ -1,6 +1,29 @@
-class TreeNode:
-    def __init__(self,x):
-        self.left=None
-        self.right=None
-        self.val=x
+# 思路：典型的栈应用
 
+def evalRPN(tokens):
+    s = list()
+
+    opt = set(['+', '-', '*', '/'])
+
+    for ch in tokens:
+        if ch in opt:
+            op2 = s.pop()
+            op1 = s.pop()
+            if ch == '+':
+                s.append(op1 + op2)
+            elif ch == '-':
+                s.append(op1 - op2)
+            elif ch == '*':
+                s.append(op1 * op2)
+            elif ch == '/':
+                if op2 == 0:
+                    s.append(0)
+                else:
+                    s.append(op1 // op2)
+        else:
+            s.append(int(ch))
+
+    return s[-1]
+
+
+evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"])
