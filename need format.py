9b71a24762bcd5902d99296dfd7a59ef7839cc90
Daya_Jin <qq435248055@gmail.com>
2019-04-02 22:34:00 +0800

    update

--- need format.py
+++ need format.py
@@ -1,29 +1,23 @@
-# 思路：典型的栈应用
-
-def evalRPN(tokens):
-    s = list()
-
-    opt = set(['+', '-', '*', '/'])
-
-    for ch in tokens:
-        if ch in opt:
-            op2 = s.pop()
-            op1 = s.pop()
-            if ch == '+':
-                s.append(op1 + op2)
-            elif ch == '-':
-                s.append(op1 - op2)
-            elif ch == '*':
-                s.append(op1 * op2)
-            elif ch == '/':
-                if op2 == 0:
-                    s.append(0)
-                else:
-                    s.append(op1 // op2)
-        else:
-            s.append(int(ch))
-
-    return s[-1]
+# 思路：动态规划的典型例子
+# 设硬币可能的面额为denomination，状态转移方程为dp[i]=min(dp[i],dp[i-denomination]+1)
+# 关键在于动态数组的初始化，将所有位置初始化为一个大值
 
 
-evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"])
+def coinChange(coins, amount: int) -> int:
+    if not coins:
+        return -1
+    if amount == 0:
+        return 0
+
+    max_val = 1e8
+    dp = [max_val for _ in range(amount + 1)]
+
+    for i in range(1, amount + 1):
+        for denomination in coins:
+            if denomination <= i:
+                dp[i] = min(dp[i], dp[i - denomination] + 1)
+
+    return dp[-1] if dp[-1] != max_val else -1
+
+
+coinChange([1, 2, 5], 11)
