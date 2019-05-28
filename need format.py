c112e3bc64c2a29889bee67f49b9dd73a2594715
Daya_Jin <qq435248055@gmail.com>
2019-05-28 22:16:43 +0800

    update

--- need format.py
+++ need format.py
@@ -1,28 +1,28 @@
-def findLUSlength(strs) -> int:
-    strs = list(set(strs))    # 去重优化
-    strs.sort(key=len, reverse=True)
-    res = -1
-
-    def isSub(s1, s2):
-        '''
-        判断s1是不是s2的一个子序列，len(s1)<=len(s2)
-        '''
-        i = 0
-        for ch in s2:
-            if ch == s1[i]:
-                i += 1
-                if i == len(s1):
-                    return True
-        return False
-
-    for i in range(len(strs)):    # 选出的字串
-        for j in range(i):    # 比较的字串
-            if i != j and isSub(strs[i], strs[j]):
-                break
-            if j == i-1:
-                res = max(res, len(strs[i]))
-
-    return res
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
 
 
-findLUSlength(["aabbcc", "aabbcc","bc","bcc","aabbccc"])
+def offline_cv(df, cat_cols='auto'):
+    X, Y = df.drop(['uId', 'age'], axis=1), df.loc[:, 'age']
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.1, stratify=Y)
+
+    lgb_train = lgb.Dataset(X_train, Y_train, categorical_feature=cat_cols)
+    lgb_val = lgb.Dataset(X_val, Y_val, categorical_feature=cat_cols, reference=lgb_train)
+
+    params = {
+        'objective': 'multiclass',
+        'num_class': 6,
+        'metric': ['multi_logloss', 'multi_error'],
+        'num_threads': 12,
+    }
+
+    gbm = lgb.train(params, lgb_train, valid_sets=lgb_val, early_stopping_rounds=10, verbose_eval=10)
+
+    f_imp_df = pd.DataFrame({
+        'feature': X_train.columns,
+        'importance': gbm.feature_importance(),
+    }).sort_values(by='importance', ascending=False)
+
+    print(f_imp_df[:20])
