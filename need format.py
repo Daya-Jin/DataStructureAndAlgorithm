6f085c71a6fbd609132e18f31d8f82b9b8f2ad4c
Daya_Jin <qq435248055@gmail.com>
2019-06-05 10:43:59 +0800

    update

--- need format.py
+++ need format.py
@@ -1,28 +1,21 @@
-from sklearn.model_selection import train_test_split
-import pandas as pd
-import numpy as np
-import lightgbm as lgb
+def maxSubarraySumCircular(A) -> int:
+    cur_max = 0
+    max_sum = A[0]
+    cur_min = 0
+    min_sum = A[0]
+
+    for num in A:
+        cur_max += num
+        max_sum = max(max_sum, cur_max)
+        if cur_max < 0:
+            cur_max = 0
+
+        cur_min += num
+        min_sum = min(min_sum, cur_min)
+        if cur_min > 0:
+            cur_min = 0
+
+    return max(max_sum, sum(A)-min_sum)
 
 
-def offline_cv(df, cat_cols='auto'):
-    X, Y = df.drop(['uId', 'age'], axis=1), df.loc[:, 'age']
-    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.1, stratify=Y)
-
-    lgb_train = lgb.Dataset(X_train, Y_train, categorical_feature=cat_cols)
-    lgb_val = lgb.Dataset(X_val, Y_val, categorical_feature=cat_cols, reference=lgb_train)
-
-    params = {
-        'objective': 'multiclass',
-        'num_class': 6,
-        'metric': ['multi_logloss', 'multi_error'],
-        'num_threads': 12,
-    }
-
-    gbm = lgb.train(params, lgb_train, valid_sets=lgb_val, early_stopping_rounds=10, verbose_eval=10)
-
-    f_imp_df = pd.DataFrame({
-        'feature': X_train.columns,
-        'importance': gbm.feature_importance(),
-    }).sort_values(by='importance', ascending=False)
-
-    print(f_imp_df[:20])
+maxSubarraySumCircular([-2, -3, -1])
