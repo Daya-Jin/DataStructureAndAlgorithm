e7ad0c47be488b5448858afc5176469a68442e1b
Daya_Jin <qq435248055@gmail.com>
2019-07-25 19:48:13 +0800

    update

--- need format.py
+++ need format.py
@@ -1,27 +1,24 @@
-def ranking(n: int) -> int:
-    if n < 0:
-        return -1
+def nextGreaterElements(nums):
+    double_nums = nums+nums
+    s = list()
+    table = dict()
 
-    dp = [[0] * n for _ in range(n)]
-    for row in range(n):
-        dp[row][0] = 1
-    for col in range(n):
-        dp[0][col] = 1
-    for idx in range(1, n):
-        dp[idx][idx] = dp[idx-1][idx-1]*(idx + 1)
+    for num in nums:
+        if not s:
+            s.append(num)
+            continue
 
-    for row in range(1, n):
-        for col in range(1, n):
-            if row==col:
-                dp[row][col]=dp[row-1][col-1]*(row+1)
-            else:
-                dp[row][col] = (col + 1) * dp[row - 1][col] + (col + 1) * dp[row - 1][col - 1]
+        while s and num > s[-1]:
+            pre_num = s.pop()
+            table[pre_num] = num
 
-    res = 0
-    for col in range(n):
-        res += dp[-1][col]
+        s.append(num)
 
-    return res
+    while s:
+        pre_num = s.pop()
+        if pre_num not in table.keys():
+            table[pre_num] = -1
 
+    return [table[num] for num in nums]
 
-print(ranking(2))
+nextGreaterElements([3,2,1])
