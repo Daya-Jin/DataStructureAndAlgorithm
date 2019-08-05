03552b49c6ec714ebb91c62198a170e8977a5370
Daya_Jin <qq435248055@gmail.com>
2019-08-05 17:01:09 +0800

    update

--- need format.py
+++ need format.py
@@ -1,24 +1,15 @@
-def networkDelayTime(times, N: int, K: int) -> int:
-    adj_table = {idx: list() for idx in range(1, N+1)}
-    for f, t, time in times:
-        adj_table[f].append((t, time))
+def canPlaceFlowers(flowerbed, n: int) -> bool:
+    cnt = 0
 
-    res = [0x7FFFFFFF]*(N+1)
-    res[K] = 0
-    q = [(K, 0)]    # (node_idx,cur_time)
-    visited = [False]*(N+1)
+    for i in range(len(flowerbed)):
+        if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == n - 1 or flowerbed[i + 1] == 0):
+            flowerbed[i] = 1
+            cnt += 1
 
-    while q:
-        vis_node, cur_time = q.pop(0)
+            if cnt >= n:
+                return True
 
-        for t, time in adj_table[vis_node]:
-            res[t] = min(res[t], cur_time+time)
-            if not visited[t]:
-                q.append((t, cur_time+time))
-
-        visited[vis_node] = True
-
-    return max(res) if max(res) < 0x7FFFFFFF else -1
+    return False
 
 
-networkDelayTime(times=[[2, 1, 1], [2, 3, 1], [3, 4, 1]], N=4, K=2)
+canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], n=2)
