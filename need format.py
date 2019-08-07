b899dafd57aed4020c84f278ac77965e45b313c7
Daya_Jin <qq435248055@gmail.com>
2019-08-07 22:27:36 +0800

    update

--- need format.py
+++ need format.py
@@ -1,15 +1,18 @@
-def canPlaceFlowers(flowerbed, n: int) -> bool:
-    cnt = 0
+def isSymmetric(root: TreeNode) -> bool:
+    if not root or (not root.left and not root.right):
+        return True
 
-    for i in range(len(flowerbed)):
-        if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == n - 1 or flowerbed[i + 1] == 0):
-            flowerbed[i] = 1
-            cnt += 1
+    if not root.left or not root.right or root.left.val != root.right.val:
+        return False
 
-            if cnt >= n:
-                return True
+    def sym(root1, root2):
+        '''
+        判断两分支是否对称
+        '''
+        if not root1 and not root2:
+            return True
+        if not root1 or not root2 or root1.val != root2.val:
+            return False
+        return sym(root1.left, root2.right) and sym(root1.right, root2.left)
 
-    return False
-
-
-canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], n=2)
+    return sym(root.left, root.right)
