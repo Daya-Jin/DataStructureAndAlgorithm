8ea7c89ec90615cf0da52470fd3af0a514043ea6
Daya_Jin <qq435248055@gmail.com>
2019-08-10 16:55:20 +0800

    update

--- need format.py
+++ need format.py
@@ -1,18 +1,27 @@
-def isSymmetric(root: TreeNode) -> bool:
-    if not root or (not root.left and not root.right):
-        return True
+import sys
 
-    if not root.left or not root.right or root.left.val != root.right.val:
-        return False
+K = int(sys.stdin.readline().strip())
+A, X, B, Y = map(int, sys.stdin.readline().split())
+M = 1000000007
 
-    def sym(root1, root2):
-        '''
-        判断两分支是否对称
-        '''
-        if not root1 and not root2:
-            return True
-        if not root1 or not root2 or root1.val != root2.val:
-            return False
-        return sym(root1.left, root2.right) and sym(root1.right, root2.left)
 
-    return sym(root.left, root.right)
+def func(K, A, X, B, Y):
+    dp = [[0] * (K + 1) for _ in range(X + Y + 1)]
+    w = [0] * (X + Y + 1)
+    for i in range(1, X + 1):
+        w[i] = A
+    for i in range(X + 1, X + Y + 1):
+        w[i] = B
+    dp[0][0] = 1
+
+    for i in range(1, X + Y + 1):
+        for j in range(0, K + 1):
+            dp[i][j] = dp[i - 1][j]
+            if j >= w[i]:
+                dp[i][j] += dp[i - 1][j - w[i]]
+                dp[i][j] %= 1000000007
+
+    return dp[-1][-1]
+
+
+print(func(K, A, X, B, Y))
