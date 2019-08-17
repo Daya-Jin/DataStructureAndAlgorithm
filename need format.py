18d5531c4146f87b991f0e0920395a8e8f4d4fd8
Daya_Jin <qq435248055@gmail.com>
2019-08-17 10:33:41 +0800

    update

--- need format.py
+++ need format.py
@@ -1,27 +1,24 @@
+def func(arr, key):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        mid = (left + right) >> 1
+        if arr[mid] > arr[left]:  # 左边有序
+            if arr[left] <= key <=arr[mid]:
+                right = mid
+            else:
+                left = mid+1
+        else:  # 右边有序
+            if arr[mid] < key <= arr[right]:
+                left=mid+1
+            else:
+                right = mid
+
+    return left
+
+
 import sys
 
-K = int(sys.stdin.readline().strip())
-A, X, B, Y = map(int, sys.stdin.readline().split())
-M = 1000000007
+arr = list(map(int, sys.stdin.readline().split()))
+key = int(sys.stdin.readline().strip())
 
-
-def func(K, A, X, B, Y):
-    dp = [[0] * (K + 1) for _ in range(X + Y + 1)]
-    w = [0] * (X + Y + 1)
-    for i in range(1, X + 1):
-        w[i] = A
-    for i in range(X + 1, X + Y + 1):
-        w[i] = B
-    dp[0][0] = 1
-
-    for i in range(1, X + Y + 1):
-        for j in range(0, K + 1):
-            dp[i][j] = dp[i - 1][j]
-            if j >= w[i]:
-                dp[i][j] += dp[i - 1][j - w[i]]
-                dp[i][j] %= 1000000007
-
-    return dp[-1][-1]
-
-
-print(func(K, A, X, B, Y))
+print(func(arr, key))
