2fb65f5fa9b008acb0fca3e399993b3e0b0fab12
Daya_Jin <qq435248055@gmail.com>
2019-08-24 18:56:22 +0800

    update

--- need format.py
+++ need format.py
@@ -1,44 +1,21 @@
-import sys
+def predictPartyVictory(senate: str) -> str:
+    q_D, q_R = list(), list()
+    n = len(senate)
+
+    for idx, ch in enumerate(senate):
+        if ch == 'R':
+            q_R.append(idx)
+        else:
+            q_D.append(idx)
+
+    while q_D and q_R:
+        idx_D, idx_R = q_D.pop(0), q_R.pop()
+        if idx_D < idx_R:
+            q_D.append(idx_D + n)
+        else:
+            q_R.append(idx_R + n)
+
+    return "Radiant" if q_R else 'Dire'
 
 
-def handle_inputs():
-    h, w = list(map(int, sys.stdin.readline().split()))
-    img = [[0] * w for _ in range(h)]
-    for row in range(h):
-        cur_row = list(map(int, sys.stdin.readline().split()))
-        img[row] = cur_row
-
-    k_size = int(sys.stdin.readline().strip())
-    kernel = [[0] * k_size for _ in range(k_size)]
-    for row in range(k_size):
-        cur_row = list(map(float, sys.stdin.readline().split()))
-        kernel[row] = cur_row
-
-    return img, kernel
-
-
-def conv_opt(row, col, img, kernel, k_size):
-    res = 0
-    for i in range(k_size):
-        for j in range(k_size):
-            res += img[row + i][col + j] * kernel[i][j]
-
-    return min(int(res), 255)
-
-
-def mat2str(mat):
-    return '\n'.join(list(map(lambda x: ' '.join(list(map(str,x))), mat)))
-
-
-if __name__ == '__main__':
-    img, kernel = handle_inputs()
-    h, w = len(img), len(img[0])
-    k_size = len(kernel)
-
-    rows, cols = h - k_size + 1, w - k_size + 1
-    res = [[0] * cols for _ in range(rows)]
-    for row in range(rows):
-        for col in range(cols):
-            res[row][col] = conv_opt(row, col, img, kernel, k_size)
-
-    print(mat2str(res))
+predictPartyVictory("DRDRR")
